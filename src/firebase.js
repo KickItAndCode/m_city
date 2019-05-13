9f7bbaaa9cda1dd28f2de722d9a8a08774bb3d6e
rob henderson <robert.henderson@slalom.com>
2019-05-12 20:36:36 -0700

    latest updates

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,7 @@ import firebase from 'firebase/app'
 import 'firebase/app'
 import 'firebase/database'
 import 'firebase/auth'
+import 'firebase/storage'
 
 const config = {
   apiKey: "AIzaSyDwJ9Ee5uF7LDaCoBnfJ42qRj36q1VUV_c",
@@ -18,9 +19,14 @@ firebase.initializeApp(config);
 const firebaseDB = firebase.database();
 const firebaseMatches = firebaseDB.ref('matches');
 const firebasePromotions = firebaseDB.ref('promotions');
+const firebaseTeams = firebaseDB.ref('teams');
+const firebasePlayers = firebaseDB.ref('players');
 
 export {
     firebase,
     firebaseMatches,
-    firebasePromotions
+    firebasePromotions,
+    firebaseTeams,
+    firebasePlayers,
+    firebaseDB
 }
